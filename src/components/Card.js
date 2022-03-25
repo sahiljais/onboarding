5e3b1bd71b0d61f2aa0b0f0b62bb925ff86f7912
Sahil Jaiswal <sahiljaiswal1398@gmail.com>
2022-03-25 20:24:25 +0530

    style - added last step design

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Button } from "./styles/Button.styled";
 import { CardStyle } from "./styles/Card.styled";
 import { CardContainer } from "./styles/Container.styled";
-import { Flex, FlexColumn } from "./styles/Flex";
+import { Flex, FlexColumn } from "./styles/Flex.styled";
 import { StyledImage } from "./styles/Image.styled";
 import { Typography } from "./styles/Typography.styled";
 
@@ -16,11 +16,11 @@ export default function Card({heading,subHeading,currentStep,updateStep,finalSte
 
     return(
         <CardContainer>
-            {finalStep ? <div style={{display:"flex",justifyContent:"center",}}> <StyledImage src="/tick-circle.svg" size="large"></StyledImage></div> : null }
+            {finalStep ? <div style={{display:"flex",justifyContent:"center"}}> <StyledImage src="/tick-circle.svg" size="large"></StyledImage></div> : null }
             <FlexColumn alignItems={"center"}>
             {heading}
             {subHeading}
-        </FlexColumn>
+            </FlexColumn>
             {finalStep ?  null : <Flex 
              justifyContent={"center"}
              padding={"2.5rem 0rem 1rem"}>
@@ -32,6 +32,7 @@ export default function Card({heading,subHeading,currentStep,updateStep,finalSte
             <Typography>Write better. Think more clearly. Stay organized.</Typography>
         </CardStyle>
         <CardStyle
+         marginRight={"0rem"}
          active={selected === 2 ? true : false} 
          onClick={() => { setSelected(2)}}>
         <StyledImage src={selected === 2 ? "/user-group-color.svg":"/user-group.svg"}></StyledImage>
@@ -39,7 +40,7 @@ export default function Card({heading,subHeading,currentStep,updateStep,finalSte
         <Typography>Wikis, docs tasks and projects, all in one place.</Typography>
     </CardStyle>
     </Flex>}
-    {finalStep ? <Button margin={"2rem 0rem 0rem"}>Launch Cutshort</Button> : <Button onClick={handleClick}>Create Workspace</Button>}
+    {finalStep ? <Button width={"332px"} margin={"2rem 0rem 0rem"}>Launch Cutshort</Button> : <Button width={"358px"} onClick={handleClick}>Create Workspace</Button>}
     </CardContainer>
     )
 }
